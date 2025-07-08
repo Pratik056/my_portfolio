d605e9f2087e2c568d877bdec47f578fb3255948
Pratik056 <pratiktamrakar56@gmail.com>
2025-07-08 18:29:29 +0545

    .

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const arrows = document.querySelectorAll('.arrow');
 const navbars = document.querySelectorAll('.navbar');
 
 
-navbars.forEach(navbar => {
-  navbar.addEventListener('click', (e) => {
-    navbars.forEach(nav => nav.classList.remove('active'));
-    e.currentTarget.classList.add('active');
-  });
-});
+// navbars.forEach(navbar => {
+//   navbar.addEventListener('click', (e) => {
+//     navbars.forEach(nav => nav.classList.remove('active'));
+//     e.currentTarget.classList.add('active');
+//   });
+// });
 
+//changing active nav
 function changeNav(){
   navbars.forEach(navbar => navbar.classList.remove('active'));
   this.classList.add('active');
 }
 
-
+navbars.forEach(navbar => navbar.addEventListener('click',changeNav));
